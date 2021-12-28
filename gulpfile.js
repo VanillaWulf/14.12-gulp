c8a988cad52c548fd03eab261a11cdd6d48716c3
reviewer <annarusva@gmail.com>
2021-12-28 23:57:43 +0500

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+var browserSync = require("browser-sync").create();
+
+gulp.task('css', function () {
+    return gulp.src('./scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest("./css"))
+});
+
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("scss/**/*.{scss,sass}", gulp.series('css', 'reload'));
+});
+
+gulp.task('reload', function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('start-gulp', gulp.series('css', 'server'));
